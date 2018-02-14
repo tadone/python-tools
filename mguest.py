872e8274ea7b25d628d8a7158b9fe99bd257a867
Tad Swider <tadone@gmail.com>
2018-02-14 10:31:14 +0100

    Add Pawel changes

--- mguest.py
+++ mguest.py
@@ -19,12 +19,31 @@ dates = str(item_list[-4]).split()      # dates (LIST)
 new_pass = item_list[-1]                # The password is the last item from the item_list
 date_til = datetime.strptime("{} {} {}".format(dates[-3], dates[-2], dates[-1]), '%d %b %Y')    # Re-Format date to standard (instead 10 Feb 2018)
 
-if date_til < datetime.now():   # Compare dates to determine if it's after date_til.
-    print('Wi-Fi Password has changed to: {}. Reconnecting...'.format(new_pass))
-    subprocess.call(['nmcli', 'device', 'wifi', 'connect', 'M-Guest', 'password', new_pass]) # subprocess.call() - each arg is a list item
-    # print(new_pass)
+# nmcli con show M-Guest --show-secrets | grep 802-11-wireless-security.psk: | awk {'print $2'}
+# scan wifi to find M-Guest
+subprocess.call(['nmcli', 'device', 'wifi', 'rescan', 'ssid', 'M-Guest'])
+# check if M-Guest network exist already
+proc = subprocess.Popen(['nmcli', 'connection', 'show', 'id', 'M-Guest'],
+                          stdout=subprocess.PIPE,
+                          stderr=subprocess.PIPE,
+                          shell=False)
+stdout, stderr = proc.communicate()
+if proc.returncode == 0:
+    cmd = 'nmcli con show M-Guest --show-secrets | grep 802-11-wireless-security.psk: | awk {\'print $2\'}'
+    proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    old_pass = stdout.decode('utf-8').strip()
+    if date_til < datetime.now() and old_pass != new_pass:
+        print('Wi-Fi Password has changed to: {}. Reconnecting...'.format(new_pass))
+        # changing wifi password
+        subprocess.call(['nmcli', 'con', 'mod', 'M-Guest', '+802-11-wireless-security.psk', new_pass])
+        # connecting to wifi
+        #subprocess.call(['nmcli', 'con', 'up', 'M-Guest'])
+    else:
+        print('Old password is still valid')
 else:
-    print('Old password is still valid')
+    # add new wifi network
+    subprocess.call(['nmcli', 'device', 'wifi', 'connect', 'M-Guest', 'password', new_pass])
 
 # print(date_til)
 # print(datetime.now())
